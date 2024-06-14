fc1af7ba3f443e1f31285fc55935d8a940da7e42
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-14 09:53:18 +0200

    greatly expand the set of binary functions

--- src/functional.hpp
+++ src/functional.hpp
@@ -2,11 +2,41 @@
 
 #include <algorithm>
 #include <cmath>
+#include <numeric> // greatest_common_divisor, least_common_multiple
 #include <vector>
 #include "liftable.hpp"
 
 namespace fl {
 
-struct Add : Liftable<Add> { template <typename T> T impl(T lhs, T rhs) const { return lhs + rhs; } };
+// Logical operators
+struct LogicalAnd : Liftable<LogicalAnd>     { template <typename T> T impl(T lhs, T rhs) const { return lhs && rhs; } };
+struct LogicalOr  : Liftable<LogicalOr>      { template <typename T> T impl(T lhs, T rhs) const { return lhs || rhs; } };
+
+// binary ops, which are both: (1) a tag which represents the operation and (2) the implementation of that operation
+struct Add          : Liftable<Add>          { template <typename T> T impl(T lhs, T rhs) const { return lhs + rhs; } };
+struct Subtract     : Liftable<Subtract>     { template <typename T> T impl(T lhs, T rhs) const { return lhs - rhs; } };
+struct Multiply     : Liftable<Multiply>     { template <typename T> T impl(T lhs, T rhs) const { return lhs * rhs; } };
+struct Divide       : Liftable<Divide>       { template <typename T> T impl(T lhs, T rhs) const { return lhs / rhs; } };
+struct Modulo       : Liftable<Modulo>       { template <typename T> T impl(T lhs, T rhs) const { return lhs % rhs; } };
+struct Power        : Liftable<Power>        { template <typename T> T impl(T base, T exp) const { return std::pow(base, exp); } };
+struct Minimum      : Liftable<Minimum>      { template <typename T> T impl(T lhs, T rhs) const { return std::min(lhs, rhs); } };
+struct Maximum      : Liftable<Maximum>      { template <typename T> T impl(T lhs, T rhs) const { return std::max(lhs, rhs); } };
+struct Equal        : Liftable<Equal>        { template <typename T> bool impl(T lhs, T rhs) const { return lhs == rhs; } };
+struct NotEqual     : Liftable<NotEqual>     { template <typename T> bool impl(T lhs, T rhs) const { return lhs != rhs; } };
+struct Less         : Liftable<Less>         { template <typename T> bool impl(T lhs, T rhs) const { return lhs < rhs; } };
+struct LessEqual    : Liftable<LessEqual>    { template <typename T> bool impl(T lhs, T rhs) const { return lhs <= rhs; } };
+struct Greater      : Liftable<Greater>      { template <typename T> bool impl(T lhs, T rhs) const { return lhs > rhs; } };
+struct GreaterEqual : Liftable<GreaterEqual> { template <typename T> bool impl(T lhs, T rhs) const { return lhs >= rhs; } };
+
+struct LeastCommonMultiple : Liftable<LeastCommonMultiple> // note that AND is a specialization of LCM for booleans
+{
+    template <typename T> T impl(T lhs, T rhs) const { return std::lcm(lhs,rhs); }
+                       bool impl(bool lhs, bool rhs) const { return lhs && rhs; } };
+
+struct GreatestCommonDivisor : Liftable<GreatestCommonDivisor> // note that OR is a specialization of GCD for booleans
+{
+    template <typename T> T impl(T lhs, T rhs) const { return std::gcd(lhs,rhs); }
+                       bool impl(bool lhs, bool rhs) const { return lhs || rhs; }
+};
 
 }
--- test/test_lift.cpp
+++ test/test_lift.cpp
@@ -50,4 +50,17 @@ TEST(vector_vector, two)
     EXPECT_EQ(r[1], 4);
 }
 
+TEST(vector_vector, logical_or)
+{
+    const std::vector<bool> a{true, false, false, true};
+    const std::vector<bool> b{true, false, true, false};
+
+    const std::vector<bool> r = fl::LogicalOr{}(a, b);
+    EXPECT_EQ(r.size(), 4);
+    EXPECT_EQ(r[0], true);
+    EXPECT_EQ(r[1], false);
+    EXPECT_EQ(r[2], true);
+    EXPECT_EQ(r[3], true);
+}
+
 }
