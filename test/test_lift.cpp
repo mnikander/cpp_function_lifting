f2fe7116f9a03c40fb5b04576d544a8b60058b7c
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-14 10:16:04 +0200

    format unit tests

--- test/test_lift.cpp
+++ test/test_lift.cpp
@@ -7,11 +7,10 @@
 
 namespace {
 
-TEST(scalar_scalar, one)
+TEST(scalar_scalar, add)
 {
     const int a = 2;
     const int b = 3;
-
     const int r = fl::Add{}(a, b);
 
     EXPECT_EQ(r, 5);
@@ -21,7 +20,6 @@ TEST(vector_vector, empty)
 {
     const std::vector<int> a{};
     const std::vector<int> b{};
-
     const std::vector<int> r = fl::Add{}(a, b);
 
     EXPECT_EQ(r.size(), 0);
@@ -31,7 +29,6 @@ TEST(vector_vector, one)
 {
     const std::vector<int> a{1};
     const std::vector<int> b{1};
-
     const std::vector<int> r = fl::Add{}(a, b);
 
     EXPECT_EQ(r.size(), 1);
@@ -42,7 +39,6 @@ TEST(vector_vector, two)
 {
     const std::vector<int> a{1, 2};
     const std::vector<int> b{1, 2};
-
     const std::vector<int> r = fl::Add{}(a, b);
 
     EXPECT_EQ(r.size(), 2);
@@ -54,8 +50,8 @@ TEST(vector_vector, logical_or)
 {
     const std::vector<bool> a{true, false, false, true};
     const std::vector<bool> b{true, false, true, false};
-
     const std::vector<bool> r = fl::LogicalOr{}(a, b);
+
     EXPECT_EQ(r.size(), 4);
     EXPECT_EQ(r[0], true);
     EXPECT_EQ(r[1], false);
