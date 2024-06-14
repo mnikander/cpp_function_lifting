ca25a29fdd492535515e26470d134fa108398206
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-14 09:40:14 +0200

    move 'Liftable' to its own header file

---
+++ src/liftable.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+namespace fl {
+
+template <typename E>
+struct Liftable
+{
+    template <typename T>
+    T operator()(T const& left, T const& right) const
+    {
+        const auto expression = *(static_cast<const E*>(this)); // downcasting to the exact type of function via CRTP
+        return expression.impl(left, right);
+    }
+
+    template <typename T>
+    auto operator()(std::vector<T> const& left, std::vector<T> const& right) const
+        -> std::vector<T>
+        // -> std::vector<decltype((*static_cast<const E*>(this))(T{}, T{}))>
+    {
+        assert(left.size() <= right.size());
+
+        const auto expression = *(static_cast<const E*>(this)); // downcasting to the exact type of function via CRTP
+        std::vector<decltype(expression(T{}, T{}))> result(left.size());
+
+        std::transform(left.cbegin(), left.cend(), right.cbegin(), result.begin(), expression);
+        // I have no idea why, but expression.impl cannot be passed here ...........^
+        // so instead it's relying on the scalar-valued overload Liftable<E>::operator()(T, T) defined above ¯\_(ツ)_/¯
+
+        return result;
+    }
+
+    // TODO: provide more overloads for unary and binary functions
+    //               vector  -> vector
+    //      (scalar, vector) -> vector
+    //      (vector, scalar) -> vector
+};
+
+}
