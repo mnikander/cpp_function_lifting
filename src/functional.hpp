b20f5d6da7823acf65ccb10a310049384d679847
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-14 07:55:42 +0200

    implement function lifting, the 'Liftable' base class still has to be specified explicitly

---
+++ src/functional.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+namespace fl {
+
+template <typename E>
+struct Liftable 
+{
+    template <typename T>
+    auto operator()(std::vector<T> const& left, std::vector<T> const& right) const
+        -> std::vector<T>
+        // -> std::vector<decltype((*static_cast<const E*>(this))(T{}, T{}))>
+    {
+        assert(left.size() <= right.size());
+        
+        const auto binary_function = *(static_cast<const E*>(this)); // downcasting to the exact type of function via CRTP
+        std::vector<decltype(binary_function(T{}, T{}))> result(left.size());
+        std::transform(left.cbegin(), left.cend(), right.cbegin(), result.begin(), binary_function);
+        return result;
+    }
+
+    // TODO: provide more overloads for unary and binary functions
+    //               vector  -> vector
+    //      (scalar, vector) -> vector
+    //      (vector, scalar) -> vector
+};
+
+struct Add : Liftable<Add> { template <typename T> T operator()(T lhs, T rhs) const { return lhs + rhs; } };
+
+}
---
+++ test/test_lift.cpp
@@ -0,0 +1,43 @@
+#include <gtest/gtest.h>
+
+#include <cmath>
+#include <functional>
+#include <vector>
+#include "../src/functional.hpp"
+
+namespace {
+
+TEST(plus, empty)
+{
+    const std::vector<int> a{};
+    const std::vector<int> b{};
+
+    const std::vector<int> r = fl::Liftable<fl::Add>{}(a, b);
+
+    EXPECT_EQ(r.size(), 0);
+}
+
+TEST(plus, zero)
+{
+    const std::vector<int> a{0};
+    const std::vector<int> b{0};
+
+    const std::vector<int> r = fl::Liftable<fl::Add>{}(a, b);
+
+    EXPECT_EQ(r.size(), 1);
+    EXPECT_EQ(r[0], 0);
+}
+
+TEST(plus, zero_one)
+{
+    const std::vector<int> a{0, 1};
+    const std::vector<int> b{0, 1};
+
+    const std::vector<int> r = fl::Liftable<fl::Add>{}(a, b);
+
+    EXPECT_EQ(r.size(), 2);
+    EXPECT_EQ(r[0], 0);
+    EXPECT_EQ(r[1], 2);
+}
+
+}
