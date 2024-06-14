76f475a0f0d0478179877eee7bfb31d146972741
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-14 09:23:39 +0200

    modify 'Liftable' to make the implementation of 'Add' and other functions less error-prone (forgetting the 'using'-statement caused very confusing error message before)

--- src/functional.hpp
+++ src/functional.hpp
@@ -10,15 +10,26 @@ template <typename E>
 struct Liftable 
 {
     template <typename T>
+    T operator()(T const& left, T const& right) const
+    {
+        const auto expression = *(static_cast<const E*>(this)); // downcasting to the exact type of function via CRTP
+        return expression.impl(left, right);
+    }
+
+    template <typename T>
     auto operator()(std::vector<T> const& left, std::vector<T> const& right) const
         -> std::vector<T>
         // -> std::vector<decltype((*static_cast<const E*>(this))(T{}, T{}))>
     {
         assert(left.size() <= right.size());
         
-        const auto binary_function = *(static_cast<const E*>(this)); // downcasting to the exact type of function via CRTP
-        std::vector<decltype(binary_function(T{}, T{}))> result(left.size());
-        std::transform(left.cbegin(), left.cend(), right.cbegin(), result.begin(), binary_function);
+        const auto expression = *(static_cast<const E*>(this)); // downcasting to the exact type of function via CRTP
+        std::vector<decltype(expression(T{}, T{}))> result(left.size());
+
+        std::transform(left.cbegin(), left.cend(), right.cbegin(), result.begin(), expression);
+        // I have no idea why, but expression.impl cannot be passed here ...........^
+        // so instead it's relying on the scalar-valued overload Liftable<E>::operator()(T, T) defined above ¯\_(ツ)_/¯
+
         return result;
     }
 
@@ -28,10 +39,6 @@ struct Liftable
     //      (vector, scalar) -> vector
 };
 
-struct Add : Liftable<Add>
-{
-    using Liftable<Add>::operator();
-    template <typename T> T operator()(T lhs, T rhs) const { return lhs + rhs; }
-};
+struct Add : Liftable<Add> { template <typename T> T impl(T lhs, T rhs) const { return lhs + rhs; } };
 
 }
