91f91f774b733f4b758825bd4d1c9ef4b71f7e93
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-14 09:39:00 +0200

    test case for scalar-scalar

--- test/test_lift.cpp
+++ test/test_lift.cpp
@@ -7,7 +7,17 @@
 
 namespace {
 
-TEST(plus, empty)
+TEST(scalar_scalar, one)
+{
+    const int a = 2;
+    const int b = 3;
+
+    const int r = fl::Add{}(a, b);
+
+    EXPECT_EQ(r, 5);
+}
+
+TEST(vector_vector, empty)
 {
     const std::vector<int> a{};
     const std::vector<int> b{};
@@ -17,27 +27,27 @@ TEST(plus, empty)
     EXPECT_EQ(r.size(), 0);
 }
 
-TEST(plus, zero)
+TEST(vector_vector, one)
 {
-    const std::vector<int> a{0};
-    const std::vector<int> b{0};
+    const std::vector<int> a{1};
+    const std::vector<int> b{1};
 
     const std::vector<int> r = fl::Add{}(a, b);
 
     EXPECT_EQ(r.size(), 1);
-    EXPECT_EQ(r[0], 0);
+    EXPECT_EQ(r[0], 2);
 }
 
-TEST(plus, zero_one)
+TEST(vector_vector, two)
 {
-    const std::vector<int> a{0, 1};
-    const std::vector<int> b{0, 1};
+    const std::vector<int> a{1, 2};
+    const std::vector<int> b{1, 2};
 
     const std::vector<int> r = fl::Add{}(a, b);
 
     EXPECT_EQ(r.size(), 2);
-    EXPECT_EQ(r[0], 0);
-    EXPECT_EQ(r[1], 2);
+    EXPECT_EQ(r[0], 2);
+    EXPECT_EQ(r[1], 4);
 }
 
 }
